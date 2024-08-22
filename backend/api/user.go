468f3eaee020ab61f656741ed47e6d63fba64eb5
Arnav Surve <arnavsurve@gmail.com>
2024-08-21 20:49:37 -0700

    split package api into multiple files

--- backend/api/user.go
+++ backend/api/user.go
@@ -137,34 +137,3 @@ func EditUser(ctx *gin.Context, store *db.PostgresStore) {
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "User successfully updated"})
 }
-
-func HandleCreateTasksTable(ctx *gin.Context, store *db.PostgresStore) {
-	userClaims := ctx.MustGet("user").(jwt.MapClaims)
-	userID := int(userClaims["id"].(float64))
-
-	requestedID := ctx.Param("id")
-	intRequestedID, err := strconv.Atoi(requestedID)
-	if err != nil || intRequestedID != userID {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Unauthorized"})
-		return
-	}
-
-	table := utils.Table{}
-	if err := ctx.ShouldBindJSON(&table); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	if table.Name == "" {
-		ctx.AbortWithStatusJSON(500, "Table name cannot be empty")
-		return
-	}
-
-	name, err := store.CreateTasksTable(requestedID, table.Name)
-	if err != nil {
-		fmt.Println(err)
-		ctx.AbortWithStatusJSON(500, "Failed to create table")
-		return
-	}
-
-	ctx.JSON(http.StatusOK, name)
-}
