05995e9bb62035b3ecd3038b2887187ad05b770e
Arnav Surve <arnavsurve@gmail.com>
2024-08-23 11:59:09 -0700

    abort create table request if table with that name already exists

--- backend/api/task.go
+++ backend/api/task.go
@@ -32,14 +32,22 @@ func HandleCreateTasksTable(ctx *gin.Context, store *db.PostgresStore) {
 		return
 	}
 	if table.Name == "" {
-		ctx.AbortWithStatusJSON(500, "Table name cannot be empty")
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Table name cannot be empty"})
+		return
+	}
+
+	// Check if table exists
+	tableName := fmt.Sprintf("t_%s_%s", requestedID, table.Name)
+	if yes, err := store.TableExists(tableName); yes == true {
+		fmt.Println(err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Table already exists with that name"})
 		return
 	}
 
 	name, err := store.CreateTasksTable(requestedID, table.Name)
 	if err != nil {
 		fmt.Println(err)
-		ctx.AbortWithStatusJSON(500, "Failed to create table")
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create tasks table"})
 		return
 	}
 
@@ -48,6 +56,7 @@ func HandleCreateTasksTable(ctx *gin.Context, store *db.PostgresStore) {
 
 // HandleCreateTask calls the CreateTask function with user ID and target table passed as URL parameters.
 // Task attributes are read from Gin context (JSON request body) and passed to CreateTask.
+// A table's name is comprised of the user's ID and name of the workspace.
 func HandleCreateTask(ctx *gin.Context, store *db.PostgresStore) {
 	userClaims := ctx.MustGet("user").(jwt.MapClaims)
 	userID := int(userClaims["id"].(float64))
@@ -61,8 +70,8 @@ func HandleCreateTask(ctx *gin.Context, store *db.PostgresStore) {
 	}
 
 	// Checking if the requested table for the task exists
-	tableDesc := ctx.Param("table")
-	requestedTable := fmt.Sprintf("t_%s_%s", requestedID, tableDesc)
+	workspaceName := ctx.Param("workspace")
+	requestedTable := fmt.Sprintf("t_%s_%s", requestedID, workspaceName)
 	exists, err := store.TableExists(requestedTable)
 	if exists != true {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Table does not exist"})
@@ -90,7 +99,8 @@ func HandleCreateTask(ctx *gin.Context, store *db.PostgresStore) {
 	ctx.JSON(http.StatusOK, gin.H{"table": name})
 }
 
-func HandleListTasks(ctx *gin.Context, store *db.PostgresStore) {
+// HandleGetTasks ... TODO!!!
+func HandleGetTasks(ctx *gin.Context, store *db.PostgresStore) {
 	userClaims := ctx.MustGet("user").(jwt.MapClaims)
 	userID := int(userClaims["id"].(float64))
 
@@ -99,6 +109,15 @@ func HandleListTasks(ctx *gin.Context, store *db.PostgresStore) {
 	intRequestedID, err := strconv.Atoi(requestedID)
 	if err != nil || intRequestedID != userID {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	// Checking if the requested table for the task exists
+	workspaceName := ctx.Param("workspace")
+	requestedTable := fmt.Sprintf("t_%s_%s", requestedID, workspaceName)
+	exists, err := store.TableExists(requestedTable)
+	if exists != true {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Table does not exist"})
 		return
 	}
 
