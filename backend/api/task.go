dd8995f121675bc9cf37592dd4677fa25795a2fc
Arnav Surve <arnavsurve@gmail.com>
2024-08-28 11:52:33 -0700

    Updating HandleCreateTask to pass a workspace ID
    Commented out HandleCreateTasksTable as logic is changing to store all
    tasks in one table.

--- backend/api/task.go
+++ backend/api/task.go
@@ -14,46 +14,46 @@ import (
 
 // HandleCreateTasksTable calls the CreateTasksTable function in db given a user's ID and name of the table.
 // On success, returns 400 and created table name.
-func HandleCreateTasksTable(ctx *gin.Context, store *db.PostgresStore) {
-	userClaims := ctx.MustGet("user").(jwt.MapClaims)
-	userID := int(userClaims["id"].(float64))
-
-	// Verify user's ID matches ID of the resource
-	requestedID := ctx.Param("id")
-	intRequestedID, err := strconv.Atoi(requestedID)
-	if err != nil || intRequestedID != userID {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-		return
-	}
-
-	// Binding JSON body to a Table struct
-	table := shared.Table{}
-	if err := ctx.ShouldBindJSON(&table); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	if table.Name == "" {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Table name cannot be empty"})
-		return
-	}
-
-	// Check if table exists
-	tableName := fmt.Sprintf("t_%s_%s", requestedID, table.Name)
-	if yes, err := store.TableExists(tableName); yes == true {
-		fmt.Println(err)
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Table already exists with that name"})
-		return
-	}
-
-	name, err := store.CreateTasksTable(requestedID, table.Name)
-	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create tasks table"})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{"table_name": name})
-}
+// func HandleCreateTasksTable(ctx *gin.Context, store *db.PostgresStore) {
+// 	userClaims := ctx.MustGet("user").(jwt.MapClaims)
+// 	userID := int(userClaims["id"].(float64))
+//
+// 	// Verify user's ID matches ID of the resource
+// 	requestedID := ctx.Param("id")
+// 	intRequestedID, err := strconv.Atoi(requestedID)
+// 	if err != nil || intRequestedID != userID {
+// 		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+// 		return
+// 	}
+//
+// 	// Binding JSON body to a Table struct
+// 	table := shared.Table{}
+// 	if err := ctx.ShouldBindJSON(&table); err != nil {
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	if table.Name == "" {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Table name cannot be empty"})
+// 		return
+// 	}
+//
+// 	// Check if table exists
+// 	tableName := fmt.Sprintf("t_%s_%s", requestedID, table.Name)
+// 	if yes, err := store.TableExists(tableName); yes == true {
+// 		fmt.Println(err)
+// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Table already exists with that name"})
+// 		return
+// 	}
+//
+// 	name, err := store.CreateTasksTable(requestedID, table.Name)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create tasks table"})
+// 		return
+// 	}
+//
+// 	ctx.JSON(http.StatusOK, gin.H{"table_name": name})
+// }
 
 // HandleCreateTask calls the CreateTask function with user ID and target table passed as URL parameters.
 // Task attributes are read from Gin context (JSON request body) and passed to CreateTask.
@@ -90,7 +90,7 @@ func HandleCreateTask(ctx *gin.Context, store *db.PostgresStore) {
 		return
 	}
 
-	name, err := store.CreateTask(requestedTable, newTask.Name, newTask.Description, newTask.DueDate, newTask.CompletionStatus, intRequestedID)
+	name, err := store.CreateTask(newTask.Name, newTask.Description, newTask.DueDate, newTask.CompletionStatus, newTask.WorkspaceID, intRequestedID)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating task"})
 		fmt.Println(err)
